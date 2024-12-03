63d061940ee149d29f8cdd0d7b10f5e2b4a36623
VincentAitzz <v.andrehpavez@gmail.com>
2024-12-03 20:30:19 -0300

    feat(auth): implement user sign-in and sign-up methods
    Added methods for user authentication using Firebase. Includes password hashing with BCrypt and storing user data in Firebase Realtime Database.

---
+++ app/src/main/java/com/vincentaitzz/bettermanagement/controllers/FirebaseManager.java
@@ -0,0 +1,54 @@
+package com.vincentaitzz.bettermanagement.controllers;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.vincentaitzz.bettermanagement.models.User;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+public class FirebaseManager {
+
+    private FirebaseAuth auth;
+    private DatabaseReference dbReference;
+
+    public FirebaseManager(){
+        auth = FirebaseAuth.getInstance();
+        dbReference = FirebaseDatabase.getInstance().getReference("users");
+    }
+    public void signIn(User userData, OnCompleteListener<AuthResult> listener){
+        auth.signInWithEmailAndPassword(userData.getEMAIL(), userData.getPASSWORD()).addOnCompleteListener(listener);
+    }
+    public void signUp(User userData, OnCompleteListener<AuthResult> listener){
+        String hashPWD = BCrypt.withDefaults().hashToString(15,userData.getPASSWORD().toCharArray());
+        auth.createUserWithEmailAndPassword(userData.getEMAIL(),hashPWD).addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                FirebaseUser firebaseUser = auth.getCurrentUser();
+                String userID = firebaseUser.getUid();
+
+                userData.setPASSWORD(hashPWD);
+
+                dbReference.child(userID).setValue(userData).addOnCompleteListener(dbTask -> {
+                    if(dbTask.isSuccessful()){
+                        listener.onComplete(task);
+                    }else{
+                        listener.onComplete(Tasks.forException(dbTask.getException()));
+                    }
+                });
+            }else{
+                listener.onComplete(task);
+            }
+        });
+    }
+
+    public void signOut(){
+        auth.signOut();
+    }
+    public FirebaseUser getCurrentUser(){
+        return auth.getCurrentUser();
+    }
+}
---
+++ app/src/main/java/com/vincentaitzz/bettermanagement/models/User.java
@@ -0,0 +1,50 @@
+package com.vincentaitzz.bettermanagement.models;
+
+public class User {
+    private String ID;
+    private String NAME;
+    private String PASSWORD;
+    private String EMAIL;
+
+    public User() {
+    }
+
+    public User(String ID, String NAME, String PASSWORD, String EMAIL) {
+        this.ID = ID;
+        this.NAME = NAME;
+        this.PASSWORD = PASSWORD;
+        this.EMAIL = EMAIL;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public void setNAME(String NAME) {
+        this.NAME = NAME;
+    }
+
+    public String getPASSWORD() {
+        return PASSWORD;
+    }
+
+    public void setPASSWORD(String PASSWORD) {
+        this.PASSWORD = PASSWORD;
+    }
+
+    public String getEMAIL() {
+        return EMAIL;
+    }
+
+    public void setEMAIL(String EMAIL) {
+        this.EMAIL = EMAIL;
+    }
+}
