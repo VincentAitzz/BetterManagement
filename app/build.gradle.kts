aca817fcaa0dcc0279e8f0bb146e9da333635250
VincentAitzz <v.andrehpavez@gmail.com>
2024-12-03 20:28:28 -0300

    feat(auth): implement user sign-in and sign-up methods
    Added methods for user authentication using Firebase. Includes password hashing with BCrypt and storing user data in Firebase Realtime Database.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
+
 }
 
 android {
@@ -33,10 +35,16 @@ android {
 
 dependencies {
 
+    implementation("at.favre.lib:bcrypt:0.9.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
