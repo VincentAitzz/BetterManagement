d7090a19587ce89a3e6dafc9f5712da67a48bc74
VincentAitzz <v.andrehpavez@gmail.com>
2024-12-03 21:01:22 -0300

    feat(ui): add splash screen and button designs
    Implemented a splash screen for the app's launch and created XML layouts for the main buttons. Enhances user experience during app startup.

--- app/src/main/java/com/vincentaitzz/bettermanagement/controllers/SplashScreen.java
+++ app/src/main/java/com/vincentaitzz/bettermanagement/controllers/SplashScreen.java
@@ -1,6 +1,10 @@
-package com.vincentaitzz.bettermanagement;
+package com.vincentaitzz.bettermanagement.controllers;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.vincentaitzz.bettermanagement.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private Button btnGetStarted;
+    private TextView signInText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnGetStarted = findViewById(R.id.getStartedButton);
+        signInText = findViewById(R.id.signInText);
+
+        btnGetStarted.setOnClickListener(v -> {
+
         });
     }
 }
