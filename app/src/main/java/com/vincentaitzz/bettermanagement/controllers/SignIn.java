f298b67bbdf31824f757597b38713ab04e6a3e80
VincentAitzz <v.andrehpavez@gmail.com>
2024-12-03 23:06:27 -0300

    feat(auth): complete user login functionality with Firebase
    mplemented user login feature using Firebase Authentication. Users can now sign in with their email and password, enhancing app security and user experience.

--- app/src/main/java/com/vincentaitzz/bettermanagement/controllers/SignIn.java
+++ app/src/main/java/com/vincentaitzz/bettermanagement/controllers/SignIn.java
@@ -1,16 +1,29 @@
 package com.vincentaitzz.bettermanagement.controllers;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.vincentaitzz.bettermanagement.R;
+import com.vincentaitzz.bettermanagement.models.User;
 
 public class SignIn extends AppCompatActivity {
+
+    private EditText txtEmail, txtPassword;
+    private Button btnSignIn;
+    private FirebaseManager auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,40 @@ public class SignIn extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        auth = new FirebaseManager();
+
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPassword = findViewById(R.id.txtPassword);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(v -> {
+            String email = txtEmail.getText().toString().trim();
+            String password = txtPassword.getText().toString().trim();
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            User userData = new User();
+            userData.setEMAIL(email);
+            userData.setPASSWORD(password);
+
+            auth.signIn(userData, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(SignIn.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(getApplicationContext(), Home.class);
+                        startActivity(i);
+                    } else {
+                        Toast.makeText(SignIn.this, "Error al iniciar sesión: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         });
     }
 }
